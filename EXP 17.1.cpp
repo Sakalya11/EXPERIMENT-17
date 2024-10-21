ded78a4368a48522c9fedd2f14f29fcdc71c2e5c
Sakalya11 <sakalya.m11@gmail.com>
2024-10-21 15:11:02 +0530

    Update EXP 17.1.cpp

--- EXP 17.1.cpp
+++ EXP 17.1.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 using namespace std;
 
@@ -9,10 +10,14 @@ class Link {
     Link(int num){
         data=num;
         next=NULL;
-    } 
+    }
+    /*void disp(){
+        cout<<data<<"   "<<next;
+    }*/
 };
 
 int main(){
     Link* l1 = new Link(6);
     cout<<l1->data<<"   "<<l1->next;
+    //l1.disp();
 }
